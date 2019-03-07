028481d34024d2704dee31e64e962de7f20a77a0
aubrin cole <dcolwell@juno.com>
2019-03-07 14:14:42 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 function bad() {
-	document.getElementById('bad').play()
 	
 }
 function soal() {
-	document.getElementById('soal').play()
 	
 }
 function jesse() {
-	document.getElementById('jesse').play()
 	
 }
